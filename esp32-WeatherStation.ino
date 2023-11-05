d6579afa80a9395e3ddd3c7d6f7df7b4fd8ab245
Davy Guilherme <dvybatista@gmail.com>
2023-11-05 10:09:50 -0300

    Medindo a temperatura em uma função externa

--- esp32-WeatherStation.ino
+++ esp32-WeatherStation.ino
@@ -1,60 +1,92 @@
 #include <WiFi.h>
-#include <WiFiClient.h>
 #include <WebServer.h>
 #include <ESPmDNS.h>
-
 #include <SimpleDHT.h>
-
-
-// WebServer
-const char* ssid = "SOCRATES";
-const char* password = "21061993";
+//
+//const char* ssid = "2.4G_Apto94";
+//const char* password = "gatinhos";
+const char* ssid = "CONEXAO LENTA 2.4G";
+const char* password = "solderachar00!";
 WebServer server(80);
 
-// DHT11
 int pinDHT11 = 23;
 SimpleDHT11 dht11(pinDHT11);
 
 int temperatura = 0;
+int humidade = 0;
 
-void handleRoot () {
 
-  // read without samples.
+void medirTemperaturaHumidade () {
+
+  Serial.println("Vai medir");
+
   byte temperature = 0;
   byte humidity = 0;
   int err = SimpleDHTErrSuccess;
+
   if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
-    Serial.print("Read DHT11 failed, err="); 
-    Serial.print(SimpleDHTErrCode(err));
-    Serial.print(","); 
-    Serial.println(SimpleDHTErrDuration(err)); 
+     Serial.print("Read DHT11 failed, err=");
+     Serial.print(SimpleDHTErrCode(err));
+     Serial.print(",");
+     Serial.println(SimpleDHTErrDuration(err));
     return;
   }
 
-  Serial.print("Sample OK: ");
-  Serial.print((int)temperature); Serial.print(" *C, "); 
-  Serial.print((int)humidity); Serial.println(" H");
+//  *temperaturaPtr = (int)temperature;
+  temperatura = (int)temperature;
+  humidade = (int)humidity;
+
+}
+void handleRoot() {
+  // byte temperature = 0;
+  // byte humidity = 0;
+  // int err = SimpleDHTErrSuccess;
+
+  // if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+  //   Serial.print("Read DHT11 failed, err=");
+  //   Serial.print(SimpleDHTErrCode(err));
+  //   Serial.print(",");
+  //   Serial.println(SimpleDHTErrDuration(err));
+  //   return;
+  // }
+
+  // temperatura = (int)temperature; // Atualiza a variável global com a temperatura atual.
+
+  medirTemperaturaHumidade ();
 
   String html = "<html><body>";
-  html += "<h1>Temperatura 2:</h1>";
-//  html += "<p>" + String((int)temperature) + " &#8451;</p>";  // Mostra o símbolo de grau Celsius
-  html += "<p>";
-  html += (int)temperature;
-//  html += (int)temperatura;
-  html += " &#8451;</p>";
+  html += "<h1>Temperatura:</h1>";
+  html += "<p id='temperatura'>";
+  html += temperatura;
+  html += " &#8451;</p>"; // rever
+  html += "<script>";
+  html += "setInterval(function() {";
+  html += "  var xhttp = new XMLHttpRequest();";
+  html += "  xhttp.onreadystatechange = function() {";
+  html += "    if (this.readyState == 4 && this.status == 200) {";
+  html += "      document.getElementById('temperatura').innerHTML = this.responseText;";
+  html += "    }";
+  html += "  };";
+  html += "  xhttp.open('GET', '/get-temperatura', true);";
+  html += "  xhttp.send();";
+  html += "}, 5000);"; // Atualiza a temperatura a cada 5 segundos.
+  html += "</script>";
   html += "</body></html>";
   server.send(200, "text/html", html);
+}
 
+void handleGetTemperatura() {
+  medirTemperaturaHumidade();
+  server.send(200, "text/plain", String(temperatura) + " &#8451;");
 }
 
 void setup() {
   Serial.begin(115200);
-  
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("");
 
-  // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -65,7 +97,7 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  if (MDNS.begin("esp32")) {
+  if (MDNS.begin("teste")) {
     Serial.println("MDNS responder started");
   }
 
@@ -75,7 +107,7 @@ void setup() {
     server.send(200, "text/plain", "this works as well");
   });
 
-//  server.onNotFound(handleNotFound);
+  server.on("/get-temperatura", HTTP_GET, handleGetTemperatura); // Adiciona um novo endpoint para buscar a temperatura.
 
   server.begin();
   Serial.println("HTTP server started");
@@ -101,7 +133,5 @@ void loop() {
 //  temperatura = (int)temperature;
 
   server.handleClient();
-  delay(2);//allow the cpu to switch to other tasks
-//delay(2000);
-  
+  delay(2);
 }
